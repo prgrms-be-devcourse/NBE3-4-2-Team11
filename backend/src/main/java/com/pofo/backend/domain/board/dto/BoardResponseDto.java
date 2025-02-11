d1d4dbe215d7b5f4c1dbef0eb3f0f5e1619b3209
pingu0118 <rnjsqhrud99@naver.com>
2025-02-11 14:39:38 +0900

    dto부분 createdAt필드만

--- backend/src/main/java/com/pofo/backend/domain/board/dto/BoardResponseDto.java
+++ backend/src/main/java/com/pofo/backend/domain/board/dto/BoardResponseDto.java
@@ -3,6 +3,8 @@ package com.pofo.backend.domain.board.dto;
 import com.pofo.backend.domain.board.entity.Board;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 
 //게시글 상세 조회 -> GET /api/v1/user/boards/{id}
 
@@ -11,11 +13,15 @@ public class BoardResponseDto {
     private final Long id;  //게시글 ID
     private final String title;
     private final String content;
+    private final LocalDateTime createdAt;  // 작성일자 필드 추가
+
 
 
     public BoardResponseDto(Board board) {
         this.id = board.getId();
         this.title = board.getTitle();
         this.content = board.getContent();
+        this.createdAt = board.getCreatedAt();  // BaseTime의 필드 값 설정
+
     }
 }
